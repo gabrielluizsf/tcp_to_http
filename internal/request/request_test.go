a8e39416a13b84771e9f45a57ede8ab3d4387c8d
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-08-20 17:25:58 -0300

    gofmt

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -9,22 +9,22 @@ import (
 
 func TestRequestLineParse(t *testing.T) {
 	reader := &chunkReader{
-		data: "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n",
+		data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n",
 		numBytesPerRead: 3,
 	}
 	r, err := NewFromReader(reader)
 	assert.NoError(t, err)
-	assert.Equal(t,r.Line.Method, "GET")
+	assert.Equal(t, r.Line.Method, "GET")
 	assert.Equal(t, r.Line.Target, "/")
 	assert.Equal(t, r.Line.Version, "1.1")
-	
+
 	reader = &chunkReader{
-		data: "GET /coffe HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n",
+		data:            "GET /coffe HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n",
 		numBytesPerRead: 1,
 	}
 	r, err = NewFromReader(reader)
 	assert.NoError(t, err)
-	assert.Equal(t,r.Line.Method, "GET")
+	assert.Equal(t, r.Line.Method, "GET")
 	assert.Equal(t, r.Line.Target, "/coffe")
 	assert.Equal(t, r.Line.Version, "1.1")
 
