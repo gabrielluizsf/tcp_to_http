162bbf9dafb9c0c09e8102d4222971f8aaa0d17d
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-08-21 17:23:46 -0300

    feat: implement WriteHeaders function for HTTP response handling

---
+++ internal/response/write_headers.go
@@ -0,0 +1,21 @@
+package response
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/gabrielluizsf/tcp_to_http/internal/headers"
+)
+
+func WriteHeaders(
+	w io.Writer,
+	h headers.Headers,
+) (err error) {
+	b := []byte{}
+	for key, value := range h {
+		b = fmt.Appendf(b, "%s: %s\r\n", key, value)
+	}
+	b = fmt.Append(b, "\r\n")
+	_, err = w.Write(b)
+	return
+}
