6e204139bebb243958e13aaaf62ae1a453fe7463
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-08-21 17:23:59 -0300

    feat: add GetDefaultHeaders function to generate default HTTP response headers

---
+++ internal/response/default_headers.go
@@ -0,0 +1,15 @@
+package response
+
+import (
+	"fmt"
+
+	"github.com/gabrielluizsf/tcp_to_http/internal/headers"
+)
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	h := headers.New()
+	h.Set("Content-Length", fmt.Sprint(contentLen))
+	h.Set("Connection", "close")
+	h.Set("Content-Type", "text/plain; charset=utf-8")
+	return h
+}
