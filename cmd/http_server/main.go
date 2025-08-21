853554e01e91119bb246c4c59a8c149d419f453a
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-08-21 18:30:04 -0300

    refactor: enhance response handling in main function with structured status responses

--- cmd/http_server/main.go
+++ cmd/http_server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -16,23 +16,24 @@ import (
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+	server, err := server.Serve(port, func(res *response.Writer, req *request.Request) {
 		endpoint := stringx.New(req.Line.Target)
+		headers := response.GetDefaultHeaders(0)
+		contentType, body := respond200()
+		statusCode := response.StatusOK
 		switch {
 		case endpoint.Includes("/yourproblem"):
-			return &server.HandlerError{
-				StatusCode: response.StatusBadRequest,
-				Message:    "Your problem is not my problem\n",
-			}
+			statusCode = response.StatusBadRequest
+			contentType, body = respond400()
 		case endpoint.Includes("/myproblem"):
-			return &server.HandlerError{
-				StatusCode: response.StatusInternalServerError,
-				Message:    "Woopsie, my bad\n",
-			}
-		default:
-			w.Write([]byte("All good, frfr\n"))
+			statusCode = response.StatusInternalServerError
+			contentType, body = respond500()
 		}
-		return nil
+		res.WriteStatusLine(statusCode)
+		headers.Replace("Content-Length", fmt.Sprint(len(body)))
+		headers.Replace("Content-Type", contentType)
+		res.WriteHeaders(headers)
+		res.WriteBody(body)
 	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
@@ -44,4 +45,48 @@ func main() {
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChan
 	log.Println("Server gracefully stopped")
+}
+
+const HTML_CONTENT_TYPE = "text/html; charset=utf-8"
+
+func respond400() (contentType string, data []byte) {
+	contentType = HTML_CONTENT_TYPE
+	data = []byte(`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`)
+	return
+}
+
+func respond500() (contentType string, data []byte) {
+	contentType = HTML_CONTENT_TYPE
+	data = []byte(`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`)
+	return
+}
+
+func respond200() (contentType string, data []byte) {
+	contentType = HTML_CONTENT_TYPE
+	data = []byte(`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`)
+	return
 }
