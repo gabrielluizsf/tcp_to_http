12cd82e8c9b699311de0130e7fac6b34e0c68949
gabrielluizsf <93672097+gabrielluizsf@users.noreply.github.com>
2025-08-21 17:24:50 -0300

    feat: update Serve function to accept a handler for custom request processing

--- cmd/http_server/main.go
+++ cmd/http_server/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/gabrielluizsf/tcp_to_http/internal/request"
+	"github.com/gabrielluizsf/tcp_to_http/internal/response"
 	"github.com/gabrielluizsf/tcp_to_http/internal/server"
+	"github.com/i9si-sistemas/stringx"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+		endpoint := stringx.New(req.Line.Target)
+		switch {
+		case endpoint.Includes("/yourproblem"):
+			return &server.HandlerError{
+				StatusCode: response.StatusBadRequest,
+				Message:    "Your problem is not my problem\n",
+			}
+		case endpoint.Includes("/myproblem"):
+			return &server.HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message:    "Woopsie, my bad\n",
+			}
+		default:
+			w.Write([]byte("All good, frfr\n"))
+		}
+		return nil
+	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
